9e633d1ba03b122bc638d80942c74764893bfb15
Reece1109 <reecesimon84@gmail.com>
2022-11-25 11:08:40 +0200

    Update

--- js/code.js
+++ js/code.js
@@ -1,4 +1,28 @@
-let cars = {car4, car6, car8, car9, car11, car14};
-let size = car.length;
-console.log(size);
-console.log(car[0])
+//Question1
+let numbers = [4, 6, 8, 9, 11, 14];
+console.log(numbers)
+
+//Question 2
+let number = numbers.length
+console.log(number)
+
+//Question 3
+let numb = [4]
+console.log(numb)
+
+//Question4
+const even = [ 4, 6, 8, 14,]
+console.log(even);
+
+const evens = [];
+for (const num of numbers) {
+    if (num % 2 === 0) {
+    evens.push(num);
+    }
+}
+
+//Question 5
+if (numbers.includes(9)) {
+    console.log('It does postionIs: ' + numbers.indexOf(9));
+}
+
